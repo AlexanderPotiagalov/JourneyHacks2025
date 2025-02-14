0d8c0b42bb378024356c489650c19b6fa8f6a25d
Manan565 <mananmehta634@gmail.com>
2025-02-14 12:24:49 -0800

    setting the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import Test from "./components/Test.jsx";
+import {
+  Route,
+  CreateBrowserRouter,
+  CreateRoutesFromElements,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
 
+import MainLayout from "./layouts/MainLayout.jsx";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+    </Route>
+  )
+);
 function App() {
   return (
     <>
-      <Test />
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/Test.jsx
+++
@@ -1,5 +0,0 @@
-const test = () => {
-  return <div className="bg-gray-100">test</div>;
-};
-
-export default test;
